8ee9d4eea9ab7f0a5ca720843fcdd89f4f3c83ba
Adam Kiss <kisssandoradam@gmail.com>
2016-01-28 00:04:40 +0100

    Basic file upload is working.

--- docroot/WEB-INF/src/hu/unideb/inf/portlet/SubjectCourseAdminPortlet.java
+++ docroot/WEB-INF/src/hu/unideb/inf/portlet/SubjectCourseAdminPortlet.java
@@ -1,11 +1,34 @@
 package hu.unideb.inf.portlet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
  * Portlet implementation class SubjectCourseAdminPortlet
  */
 public class SubjectCourseAdminPortlet extends MVCPortlet {
- 
+
+	private final static String fileInputName = "fileupload";
+
+	public void upload(ActionRequest request, ActionResponse response) throws Exception {
+		UploadPortletRequest uploadRequest = PortalUtil.getUploadPortletRequest(request);
+
+		File uploadedFile = uploadRequest.getFile(fileInputName);
+
+		try (BufferedReader br = new BufferedReader(new FileReader(uploadedFile))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		}
+	}
 
 }
--- docroot/html/subjectcourseadmin/view.jsp
+++ docroot/html/subjectcourseadmin/view.jsp
@@ -1,5 +1,19 @@
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui"%>
+<%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui"%>
 
 <portlet:defineObjects />
 
 This is the <b>Subject Course Admin</b> portlet in View mode.
+
+<!-- File upload form from http://www.codeyouneed.com/liferay-portlet-file-upload-tutorial/ -->
+
+<portlet:actionURL name="upload" var="uploadFileURL"></portlet:actionURL>
+ 
+<aui:form action="<%= uploadFileURL %>" enctype="multipart/form-data" method="post">
+ 
+	<aui:input type="file" name="fileupload" />
+	
+	<aui:button name="Save" value="Save" type="submit" />
+ 
+</aui:form>
