b75aaa7c5db4c4a5b42591025c7f98763b806968
Adam Kiss <kisssandoradam@gmail.com>
2016-01-27 23:45:10 +0100

    Added new empty portlet: Subject Course Admin.

---
+++ docroot/WEB-INF/src/hu/unideb/inf/portlet/SubjectCourseAdminPortlet.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class SubjectCourseAdminPortlet
+ */
+public class SubjectCourseAdminPortlet extends MVCPortlet {
+ 
+
+}
