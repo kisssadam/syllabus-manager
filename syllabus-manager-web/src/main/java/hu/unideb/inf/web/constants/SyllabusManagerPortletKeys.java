cbf7d0efd3bbf4d813c8b7e20864597f3580e1c8
Adam Kiss <kisssandoradam@gmail.com>
2017-01-28 22:44:22 +0100

    #30 - something broke in the previous commit

--- syllabus-manager-web/src/main/java/hu/unideb/inf/web/constants/SyllabusManagerPortletKeys.java
+++ syllabus-manager-web/src/main/java/hu/unideb/inf/web/constants/SyllabusManagerPortletKeys.java
@@ -2,6 +2,6 @@ package hu.unideb.inf.web.constants;
 
 public class SyllabusManagerPortletKeys {
 
-	public static final String SYLLABUS_MANAGER_ADMIN = "SYLLABUS_MANAGER_ADMIN";
+	public static final String SYLLABUS_MANAGER_ADMIN = "SyllabusManagerAdmin";
 
 }
